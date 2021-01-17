4323b295023e142c616e81c9238921e0d47d2aec
hujs888 <hujs@colla.cc>
2021-01-18 00:51:13 +0800

    pbft

--- p2p/dht/service/myselfpeer.go
+++ p2p/dht/service/myselfpeer.go
@@ -2,8 +2,11 @@ package service
 
 import (
 	"github.com/curltech/go-colla-core/cache"
+	entity2 "github.com/curltech/go-colla-core/entity"
 	"github.com/curltech/go-colla-core/service"
 	"github.com/curltech/go-colla-core/util/message"
+	"sync"
+
 	//"github.com/curltech/go-colla-core/p2p"
 	"github.com/curltech/go-colla-node/p2p/dht/entity"
 )
@@ -13,6 +16,7 @@ import (
 */
 type MyselfPeerService struct {
 	PeerEntityService
+	Mutex sync.Mutex
 }
 
 var myselfPeerService = &MyselfPeerService{}
@@ -59,4 +63,37 @@ func init() {
 	myselfPeerService.OrmBaseService.GetSeqName = myselfPeerService.GetSeqName
 	myselfPeerService.OrmBaseService.FactNewEntity = myselfPeerService.NewEntity
 	myselfPeerService.OrmBaseService.FactNewEntities = myselfPeerService.NewEntities
+}
+
+func (this *MyselfPeerService) getCacheKey() string {
+	return "MyselfPeer"
+}
+
+func (this *MyselfPeerService) GetFromCache(refreshs ...bool) *entity.MyselfPeer {
+	key := this.getCacheKey()
+	ptr, found := MemCache.Get(key)
+	refresh := false
+	if refreshs != nil && len(refreshs) > 0 {
+		refresh = refreshs[0]
+	}
+	if found && !refresh {
+		return ptr.(*entity.MyselfPeer)
+	}
+	this.Mutex.Lock()
+	defer this.Mutex.Unlock()
+	ptr, found = MemCache.Get(key)
+	if !found || refresh {
+		myselfPeer := entity.MyselfPeer{}
+		myselfPeer.Status = entity2.EntityStatus_Effective
+		found = this.Get(&myselfPeer, false, "", "")
+		if found {
+			ptr = &myselfPeer
+		} else {
+			ptr = &entity.MyselfPeer{}
+		}
+
+		MemCache.SetDefault(key, ptr)
+	}
+
+	return ptr.(*entity.MyselfPeer)
 }
