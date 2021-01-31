1c9e04275ec5b79a0f099c9680b42ce052e99650
visualfengs <65856788+visualfengs@users.noreply.github.com>
2021-02-01 00:33:54 +0800

    Update peerclient.go

--- p2p/dht/service/peerclient.go
+++ p2p/dht/service/peerclient.go
@@ -8,7 +8,7 @@ import (
 	"github.com/curltech/go-colla-node/libp2p/ns"
 	"github.com/curltech/go-colla-node/p2p/dht/entity"
 	"github.com/curltech/go-colla-core/config"
-	"github.com/curltech/go-colla-core/p2p/msg"
+	"github.com/curltech/go-colla-node/p2p/msg"
 	"github.com/curltech/go-colla-core/logger"
 	"sync"
 	"errors"
