3f9af9e3b66c592805d59ec1212ad49f0baa0256
visualfengs <65856788+visualfengs@users.noreply.github.com>
2021-02-25 22:06:51 +0800

    Update default.go

--- p2p/chain/handler/sender/default.go
+++ p2p/chain/handler/sender/default.go
@@ -68,7 +68,7 @@ func send(msg *msg1.ChainMessage) (*msg1.ChainMessage, error) {
 				logger.Sugar.Infof("Write data length:%v", len(data))
 				_, _, err = pipe.Write(data, false)
 				if err != nil {
-					logger.Sugar.Errorf("pipe.Write failure")
+					logger.Sugar.Errorf("pipe.Write failure: %v", err)
 					return nil, err
 				}
 			} else {
