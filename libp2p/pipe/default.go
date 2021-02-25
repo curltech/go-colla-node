c879db8f45279827c61e77ad93630002c0759a58
visualfengs <65856788+visualfengs@users.noreply.github.com>
2021-02-25 17:22:22 +0800

    Update default.go

--- libp2p/pipe/default.go
+++ libp2p/pipe/default.go
@@ -113,7 +113,7 @@ func (pipe *Pipe) Write(data []byte, sync bool) (*Pipe, <-chan []byte, error) {
 	logger.Sugar.Infof("streamId:%v, connId:%v", streamId, connId)
 	_, err = pipe.stream.Write(data)
 	if err != nil {
-		logger.Sugar.Errorf("Error writing to buffer")
+		logger.Sugar.Errorf("Error writing to buffer: %v", err)
 
 		return pipe, nil, err
 	}
