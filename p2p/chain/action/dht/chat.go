e6c3bae68a4668ca0af4b338117fa2ac56fa1781
visualfengs <65856788+visualfengs@users.noreply.github.com>
2021-02-26 15:30:35 +0800

    Update chat.go

--- p2p/chain/action/dht/chat.go
+++ p2p/chain/action/dht/chat.go
@@ -3,7 +3,6 @@ package dht
 import (
 	"github.com/curltech/go-colla-node/p2p/chain/action"
 	"github.com/curltech/go-colla-node/p2p/chain/handler"
-	"github.com/curltech/go-colla-node/p2p/msg"
 	"github.com/curltech/go-colla-node/p2p/msgtype"
 )
 
@@ -16,17 +15,11 @@ var ChatAction chatAction
 /**
 在chain目录下的采用自定义protocol "/chain"的方式自己实现的功能
 */
-func (this *chatAction) Chat(peerId string, payloadType string, data interface{}, targetPeerId string, targetConnectSessionId string) (interface{}, error) {
-	chainMessage := msg.ChainMessage{}
-	chainMessage.TargetPeerId = targetPeerId
+func (this *chatAction) Chat(peerId string, data interface{}, targetPeerId string, targetConnectSessionId string) (interface{}, error) {
+	chainMessage := this.PrepareSend(peerId, data, targetPeerId)
 	chainMessage.TargetConnectSessionId = targetConnectSessionId
-	chainMessage.Payload = data
-	chainMessage.ConnectPeerId = peerId
-	chainMessage.PayloadType = payloadType
-	chainMessage.MessageType = msgtype.CHAT
-	chainMessage.MessageDirect = msgtype.MsgDirect_Request
 
-	response, err := this.Send(&chainMessage)
+	response, err := this.Send(chainMessage)
 	if err != nil {
 		return nil, err
 	}
