ae3befa032d46bd504aa1c4dfa6f76ca069682a2
visualfengs <65856788+visualfengs@users.noreply.github.com>
2021-02-26 16:09:30 +0800

    Update signal.go

--- p2p/chain/action/dht/signal.go
+++ p2p/chain/action/dht/signal.go
@@ -36,16 +36,10 @@ func (this *signalAction) RegistReceiver(name string, receiver func(peer *p2p.Ne
 /**
 peerId如果为空，发送的对象是自己，需要检查如果是自己，则检查最终目标，考虑转发
 */
-func (this *signalAction) Signal(peerId string, payloadType string, data interface{}, targetPeerId string) (interface{}, error) {
-	chainMessage := msg.ChainMessage{}
-	chainMessage.TargetPeerId = targetPeerId
-	chainMessage.Payload = data
-	chainMessage.ConnectPeerId = peerId
-	chainMessage.PayloadType = payloadType
-	chainMessage.MessageType = msgtype.SIGNAL
-	chainMessage.MessageDirect = msgtype.MsgDirect_Request
+func (this *signalAction) Signal(peerId string, data interface{}, targetPeerId string) (interface{}, error) {
+	chainMessage := this.PrepareSend(peerId, data, targetPeerId)
 
-	response, err := this.Send(&chainMessage)
+	response, err := this.Send(chainMessage)
 	if err != nil {
 		return nil, err
 	}
