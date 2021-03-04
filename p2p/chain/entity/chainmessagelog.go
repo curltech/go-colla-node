57b6b56e9d74fe523f1718219c42438f0991aa6b
visualfengs <65856788+visualfengs@users.noreply.github.com>
2021-03-04 15:37:31 +0800

    Update chainmessagelog.go

--- p2p/chain/entity/chainmessagelog.go
+++ p2p/chain/entity/chainmessagelog.go
@@ -7,18 +7,18 @@ import (
 
 type ChainMessageLog struct {
 	entity.StatusEntity `xorm:"extends"`
-	UUID                string `xorm:"varchar(255)" json:",omitempty"`
-	MessageType         string `xorm:"varchar(255)" json:",omitempty"`
-	Action              string `xorm:"varchar(255)" json:",omitempty"`
+	UUID                string `xorm:"varchar(255)" json:"uuid,omitempty"`
+	MessageType         string `xorm:"varchar(255)" json:"messageType,omitempty"`
+	Action              string `xorm:"varchar(255)" json:"action,omitempty"`
 	// 消息发送的源地址
-	SrcPeerId string `xorm:"varchar(255)" json:",omitempty"`
+	SrcPeerId string `xorm:"varchar(255)" json:"srcPeerId,omitempty"`
 	// 消息发送的目标地址
-	TargetPeerId string `xorm:"varchar(255)" json:",omitempty"`
+	TargetPeerId string `xorm:"varchar(255)" json:"targetPeerId,omitempty"`
 	// 消息发送的源地址
-	SrcAddress string `xorm:"varchar(255)" json:",omitempty"`
+	SrcAddress string `xorm:"varchar(255)" json:"srcAddress,omitempty"`
 	// 消息发送的目标地址
-	TargetAddress string `xorm:"varchar(255)" json:",omitempty"`
-	Payload       string `xorm:"varchar(10485760)" json:",omitempty"`
+	TargetAddress string `xorm:"varchar(255)" json:"targetAddress,omitempty"`
+	Payload       string `xorm:"varchar(10485760)" json:"payload,omitempty"`
 	/**
 	 * 定义消息的负载的类型
 	 *
@@ -29,15 +29,14 @@ type ChainMessageLog struct {
 	 * 其取值来源于消息构造时的设置，如果没有设置，默认为消息类型的实体类型
 	 *
 	 */
-	PayloadClass string `xorm:"varchar(255)" json:",omitempty"`
+	PayloadClass string `xorm:"varchar(255)" json:"payloadClass,omitempty"`
 	// 请求的时间戳
-	CreateTimestamp      time.Time
-	CreateTimestampNanos uint64
+	CreateTimestamp      time.Time `json:"createTimestamp,omitempty"`
 	// 返回消息
-	ResponseType    string `xorm:"varchar(255)" json:",omitempty"`
-	ResponseCode    string `xorm:"varchar(255)" json:",omitempty"`
-	ResponseMessage string `xorm:"varchar(10485760)" json:",omitempty"`
-	ResponsePayload string `xorm:"varchar(10485760)" json:",omitempty"`
+	ResponseType    string `xorm:"varchar(255)" json:"responseType,omitempty"`
+	ResponseCode    string `xorm:"varchar(255)" json:"responseCode,omitempty"`
+	ResponseMessage string `xorm:"varchar(10485760)" json:"responseMessage,omitempty"`
+	ResponsePayload string `xorm:"varchar(10485760)" json:"responsePayload,omitempty"`
 }
 
 func (ChainMessageLog) TableName() string {
