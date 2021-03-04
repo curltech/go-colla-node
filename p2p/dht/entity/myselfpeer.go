b4caeaa50009e5dfe0eafa550e078cee1fead214
visualfengs <65856788+visualfengs@users.noreply.github.com>
2021-03-04 23:50:06 +0800

    Update myselfpeer.go

--- p2p/dht/entity/myselfpeer.go
+++ p2p/dht/entity/myselfpeer.go
@@ -7,8 +7,8 @@ import (
 
 type MyselfPeer struct {
 	PeerEntity     `xorm:"extends"`
-	PeerPrivateKey string `xorm:"varchar(128)" json:"-,omitempty"`
-	PrivateKey     string `xorm:"varchar(1024)" json:"-,omitempty"`
+	PeerPrivateKey string `xorm:"varchar(128)" json:"-"`
+	PrivateKey     string `xorm:"varchar(1024)" json:"-"`
 	/**
 	 * 以下的字段是和证书相关，不是必须的
 	 */
@@ -21,8 +21,8 @@ type MyselfPeer struct {
 	// peer的新证书的密码，申请新证书的时候必须提供，不保存数据库
 	NewCertPassword string `xorm:"-" json:"newCertPassword,omitempty"`
 	// peer的hash后密码
-	//Password    string `xorm:"varchar(255)" json:"-,omitempty"`
-	CertContent string `xorm:"varchar(255)" json:"-,omitempty"`
+	//Password    string `xorm:"-" json:"-"`
+	CertContent string `xorm:"varchar(255)" json:"-"`
 	/**
 	 * 主发现地址，表示可信的，可以推荐你的peer地址
 	 */
