5d7bdd81aa0dbe6dfdbf75a14d1f0dfab10b0a65
visualfengs <visualfengs@gmail.com>
2021-12-30 03:38:28 +0000

    put old key

--- libp2p/datastore/xorm/default.go
+++ libp2p/datastore/xorm/default.go
@@ -131,7 +131,7 @@ func (this *XormDatastore) Put(key datastore.Key, value []byte) (err error) {
 				return errors.New("NoBusinessNumber")
 			}
 			reflect.SetValue(old, "BusinessNumber", businessNumber)
-		} else { // e.g. namespace == PeerEndpoint_Prefix
+		} else { // e.g. namespace == ns.PeerEndpoint_Prefix
 			reflect.SetValue(old, req.Keyname, keyvalue)
 		}
 		currentTime := time.Now()
