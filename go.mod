0b906801b863ec95f6f6e1bbeac97936c4adca00
hujs888 <hujs@colla.cc>
2023-10-27 06:02:47 +0800

    livekit service

--- go.mod
+++ go.mod
@@ -57,10 +57,23 @@ require (
 )
 
 require (
+	cloud.google.com/go v0.110.0 // indirect
+	cloud.google.com/go/compute v1.19.1 // indirect
+	cloud.google.com/go/compute/metadata v0.2.3 // indirect
+	cloud.google.com/go/firestore v1.9.0 // indirect
+	cloud.google.com/go/iam v0.13.0 // indirect
+	cloud.google.com/go/longrunning v0.4.1 // indirect
+	cloud.google.com/go/storage v1.30.1 // indirect
+	firebase.google.com/go/v4 v4.12.0 // indirect
 	github.com/BurntSushi/toml v1.1.0 // indirect
+	github.com/MicahParks/keyfunc v1.9.0 // indirect
 	github.com/btcsuite/btcd/btcec/v2 v2.3.2 // indirect
 	github.com/btcsuite/btcd/chaincfg/chainhash v1.0.2 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/google/go-cmp v0.5.9 // indirect
 	github.com/google/pprof v0.0.0-20230705174524-200ffdc848b8 // indirect
+	github.com/googleapis/enterprise-certificate-proxy v0.2.3 // indirect
+	github.com/googleapis/gax-go/v2 v2.8.0 // indirect
 	github.com/hashicorp/golang-lru/v2 v2.0.4 // indirect
 	github.com/holiman/uint256 v1.2.3 // indirect
 	github.com/ipfs/boxo v0.10.0 // indirect
@@ -84,7 +97,15 @@ require (
 	go.opentelemetry.io/otel/trace v1.16.0 // indirect
 	go.uber.org/dig v1.17.0 // indirect
 	go.uber.org/fx v1.20.0 // indirect
+	golang.org/x/oauth2 v0.7.0 // indirect
+	golang.org/x/time v0.3.0 // indirect
+	golang.org/x/xerrors v0.0.0-20220907171357-04be3eba64a2 // indirect
 	gonum.org/v1/gonum v0.13.0 // indirect
+	google.golang.org/api v0.114.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/appengine/v2 v2.0.2 // indirect
+	google.golang.org/genproto v0.0.0-20230526161137-0005af68ea54 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20230525234035-dd9d682886f9 // indirect
 )
 
 require (
@@ -126,7 +147,7 @@ require (
 	github.com/goccy/go-json v0.9.11 // indirect
 	github.com/godbus/dbus/v5 v5.1.0 // indirect
 	github.com/goinggo/mapstructure v0.0.0-20140717182941-194205d9b4a9 // indirect
-	github.com/golang-jwt/jwt/v4 v4.4.2 // indirect
+	github.com/golang-jwt/jwt/v4 v4.5.0 // indirect
 	github.com/golang/mock v1.6.0 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/golang/snappy v0.0.5-0.20220116011046-fa5810519dcb // indirect
--- livekit/default.go
+++ livekit/default.go
@@ -11,33 +11,21 @@ import (
 	"time"
 )
 
-type RoomServiceClient struct {
-	roomClient *lksdk.RoomServiceClient
-}
+// 1. install livekit server
+// macos: brew install livekit
+// linux: curl -sSL https://get.livekit.io | bash
+// windows: 在https://github.com/livekit/livekit/releases/tag/v1.5.0下载可执行文件
+// 2. start livekit
+// livekit-server --config livekit-config-sample.yaml  配置文件的启动方式
+// 里面设置了apiKey，apiSecret，turn配置，redis配置
 
-// NewRoomServiceClient 创建房间服务的客户端，这个客户端连接到livekit sfu服务器
-// 有权限创建房间，因此需要相应的APIKey和APISecret
-func NewRoomServiceClient(host string, connectInfo lksdk.ConnectInfo) *RoomServiceClient {
-	roomClient := lksdk.NewRoomServiceClient(host, connectInfo.APIKey, connectInfo.APISecret)
-
-	return &RoomServiceClient{roomClient}
-}
-
-// CreateRoom 创建新的房间，
-func (this *RoomServiceClient) CreateRoom(roomId string, maxParticipants uint32) (*livekit.Room, error) {
-	return this.roomClient.CreateRoom(context.Background(), &livekit.CreateRoomRequest{
-		Name:            roomId,
-		MaxParticipants: maxParticipants,
-		EmptyTimeout:    240 * 60, // 240 minutes
-	})
-}
-
-// CreateToken 创建新的token
-func (this *RoomServiceClient) CreateToken(roomId, identity string) (string, error) {
-	at := this.roomClient.CreateToken()
+// GetJoinToken 获取服务器的连接token，需要apiKey，apiSecret，房间名，身份名
+// 返回一个jwt的字符串token，被授权进入该房间
+func GetJoinToken(apiKey, apiSecret, roomName, identity string) (string, error) {
+	at := auth.NewAccessToken(apiKey, apiSecret)
 	grant := &auth.VideoGrant{
 		RoomJoin: true,
-		Room:     roomId,
+		Room:     roomName,
 	}
 	at.AddGrant(grant).
 		SetIdentity(identity).
@@ -46,52 +34,142 @@ func (this *RoomServiceClient) CreateToken(roomId, identity string) (string, err
 	return at.ToJWT()
 }
 
-// ListRooms 列出房间
-func (this *RoomServiceClient) ListRooms() (*livekit.ListRoomsResponse, error) {
-	return this.roomClient.ListRooms(context.Background(), &livekit.ListRoomsRequest{})
+// RoomServiceClient 连接livekit服务器的客户端，可以创建房间，对服务器的房间进行管理
+// 可以在控制层发布成restful服务
+type RoomServiceClient struct {
+	roomClient *lksdk.RoomServiceClient
+}
+
+// NewRoomServiceClient 创建房间服务的客户端，这个客户端连接到livekit sfu服务器
+// 有权限创建房间，因此需要相应的APIKey和APISecret
+func NewRoomServiceClient(host string, apiKey string, secretKey string) *RoomServiceClient {
+	roomClient := lksdk.NewRoomServiceClient(host, apiKey, secretKey)
+
+	return &RoomServiceClient{roomClient}
+}
+
+// CreateRoom 创建新的房间，
+func (svc *RoomServiceClient) CreateRoom(roomName string, emptyTimeout uint32, maxParticipants uint32, nodeId string) (*livekit.Room, error) {
+	return svc.roomClient.CreateRoom(context.Background(), &livekit.CreateRoomRequest{
+		Name:            roomName,
+		MaxParticipants: maxParticipants,
+		EmptyTimeout:    emptyTimeout, //240 * 60, // 240 minutes
+		NodeId:          nodeId,
+	})
+}
+
+// CreateToken 创建新的token，这个token在客户端连接房间的时候要使用
+// 所以每个参与者都会有一个token
+func (svc *RoomServiceClient) CreateToken(roomName, identity string, name string, ttl time.Duration) (string, error) {
+	at := svc.roomClient.CreateToken()
+	grant := &auth.VideoGrant{
+		RoomJoin: true,
+		Room:     roomName,
+	}
+	at.AddGrant(grant).
+		SetIdentity(identity).SetName(name).
+		SetValidFor(ttl)
+
+	return at.ToJWT()
 }
 
 // DeleteRoom 删除房间，所以参与人离开
-func (this *RoomServiceClient) DeleteRoom(roomId string) (*livekit.DeleteRoomResponse, error) {
-	return this.roomClient.DeleteRoom(context.Background(), &livekit.DeleteRoomRequest{
+func (svc *RoomServiceClient) DeleteRoom(roomId string) (*livekit.DeleteRoomResponse, error) {
+	return svc.roomClient.DeleteRoom(context.Background(), &livekit.DeleteRoomRequest{
 		Room: roomId,
 	})
 }
 
-// ListParticipants 列出房间的参与人
-func (this *RoomServiceClient) ListParticipants(roomId string) (*livekit.ListParticipantsResponse, error) {
-	return this.roomClient.ListParticipants(context.Background(), &livekit.ListParticipantsRequest{
-		Room: roomId,
+// ListRooms 列出房间
+func (svc *RoomServiceClient) ListRooms(roomNames []string) (*livekit.ListRoomsResponse, error) {
+	return svc.roomClient.ListRooms(context.Background(), &livekit.ListRoomsRequest{
+		Names: roomNames,
 	})
 }
 
 // GetParticipant 列出房间的参与人的详细信息
-func (this *RoomServiceClient) GetParticipant(roomId string, identity string) (*livekit.ParticipantInfo, error) {
-	return this.roomClient.GetParticipant(context.Background(), &livekit.RoomParticipantIdentity{
-		Room:     roomId,
+func (svc *RoomServiceClient) GetParticipant(roomName string, identity string) (*livekit.ParticipantInfo, error) {
+	return svc.roomClient.GetParticipant(context.Background(), &livekit.RoomParticipantIdentity{
+		Room:     roomName,
 		Identity: identity,
 	})
 }
 
+// ListParticipants 列出房间的参与人
+func (svc *RoomServiceClient) ListParticipants(roomName string) (*livekit.ListParticipantsResponse, error) {
+	return svc.roomClient.ListParticipants(context.Background(), &livekit.ListParticipantsRequest{
+		Room: roomName,
+	})
+}
+
 // RemoveParticipant 参与人从房间离开
-func (this *RoomServiceClient) RemoveParticipant(roomId string, identity string) (*livekit.RemoveParticipantResponse, error) {
-	return this.roomClient.RemoveParticipant(context.Background(), &livekit.RoomParticipantIdentity{
-		Room:     roomId,
+func (svc *RoomServiceClient) RemoveParticipant(roomName string, identity string) (*livekit.RemoveParticipantResponse, error) {
+	return svc.roomClient.RemoveParticipant(context.Background(), &livekit.RoomParticipantIdentity{
+		Room:     roomName,
 		Identity: identity,
 	})
 }
 
 // MutePublishedTrack 关闭打开轨道的声音
-func (this *RoomServiceClient) MutePublishedTrack(roomId string, identity string, trackSid string, muted bool) (*livekit.MuteRoomTrackResponse, error) {
-	return this.roomClient.MutePublishedTrack(context.Background(), &livekit.MuteRoomTrackRequest{
-		Room:     roomId,
+func (svc *RoomServiceClient) MutePublishedTrack(roomName string, identity string, trackSid string, muted bool) (*livekit.MuteRoomTrackResponse, error) {
+	return svc.roomClient.MutePublishedTrack(context.Background(), &livekit.MuteRoomTrackRequest{
+		Room:     roomName,
 		Identity: identity,
 		TrackSid: trackSid,
 		Muted:    muted,
 	})
 }
 
+// 连接房间的回调函数，返回远程流，轨道和参与者
+func onTrackSubscribed(track *webrtc.TrackRemote, publication *lksdk.RemoteTrackPublication, rp *lksdk.RemoteParticipant) {
+	fileName := fmt.Sprintf("%s-%s", rp.Identity(), track.ID())
+	fmt.Println("write track to file ", fileName)
+}
+
+// Connect 连接房间，设置回调函数
+func (svc *RoomServiceClient) Connect(host string, apiKey string, apiSecret string, roomName string, identity string) (*lksdk.Room, error) {
+	roomCallback := &lksdk.RoomCallback{
+		ParticipantCallback: lksdk.ParticipantCallback{
+			OnTrackSubscribed: onTrackSubscribed,
+		},
+	}
+	room, err := lksdk.ConnectToRoom(host, lksdk.ConnectInfo{
+		APIKey:              apiKey,
+		APISecret:           apiSecret,
+		RoomName:            roomName,
+		ParticipantIdentity: identity,
+	}, roomCallback)
+	if err != nil {
+		panic(err)
+	}
+
+	return room, err
+}
+
+// PublishTrack 把文件或者流轨道发布到房间
+func (svc *RoomServiceClient) PublishTrack(room *lksdk.Room, name string, track *lksdk.LocalSampleTrack, videoWidth int, videoHeight int) (*lksdk.LocalTrackPublication, error) {
+	return room.LocalParticipant.PublishTrack(track, &lksdk.TrackPublicationOptions{
+		Name:        name,
+		VideoWidth:  videoWidth,
+		VideoHeight: videoHeight,
+	})
+}
+
+// Disconnect 断开房间的连接
+func (svc *RoomServiceClient) Disconnect(room *lksdk.Room) {
+	room.Disconnect()
+}
+
 // NewLocalFileTrack 视频文件变成轨道
+//
+//	ffmpeg -i <input.mp4> \
+//	 -c:v libvpx -keyint_min 120 -qmax 50 -maxrate 2M -b:v 1M <output.ivf> \
+//	 -c:a libopus -page_duration 20000 -vn <output.ogg>
+//
+//	ffmpeg -i <input.mp4> \
+//	 -c:v libx264 -bsf:v h264_mp4toannexb -b:v 2M -profile baseline -pix_fmt yuv420p \
+//	   -x264-params keyint=120 -max_delay 0 -bf 0 <output.h264> \
+//	 -c:a libopus -page_duration 20000 -vn <output.ogg>
 func NewLocalFileTrack(file string) (*lksdk.LocalSampleTrack, error) {
 	return lksdk.NewLocalFileTrack(file,
 		// control FPS to ensure synchronization
@@ -106,38 +184,4 @@ func NewLocalReaderTrack(in io.ReadCloser, mime string) (*lksdk.LocalSampleTrack
 		lksdk.ReaderTrackWithFrameDuration(33*time.Millisecond),
 		lksdk.ReaderTrackWithOnWriteComplete(func() { fmt.Println("track finished") }),
 	)
-}
-
-// PublishTrack 把文件或者流轨道发布到房间
-func PublishTrack(room *lksdk.Room, name string, track *lksdk.LocalSampleTrack, videoWidth int, videoHeight int) (*lksdk.LocalTrackPublication, error) {
-	return room.LocalParticipant.PublishTrack(track, &lksdk.TrackPublicationOptions{
-		Name:        name,
-		VideoWidth:  videoWidth,
-		VideoHeight: videoHeight,
-	})
-}
-
-// ConnectToRoom 连接房间，设置回调函数，可以跟踪房间
-func ConnectToRoom(host string, connectInfo *lksdk.ConnectInfo) (*lksdk.Room, error) {
-	if host == "" || connectInfo.APIKey == "" || connectInfo.APISecret == "" || connectInfo.RoomName == "" || connectInfo.ParticipantIdentity == "" {
-		fmt.Println("invalid arguments.")
-		return nil, nil
-	}
-	room, err := lksdk.ConnectToRoom(host, *connectInfo, &lksdk.RoomCallback{
-		ParticipantCallback: lksdk.ParticipantCallback{
-			OnTrackSubscribed: onTrackSubscribed,
-		},
-	})
-
-	return room, err
-}
-
-// Disconnect 断开房间的连接
-func Disconnect(room *lksdk.Room) {
-	room.Disconnect()
-}
-
-func onTrackSubscribed(track *webrtc.TrackRemote, publication *lksdk.RemoteTrackPublication, rp *lksdk.RemoteParticipant) {
-	fileName := fmt.Sprintf("%s-%s", rp.Identity(), track.ID())
-	fmt.Println("write track to file ", fileName)
 }
